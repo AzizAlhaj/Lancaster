e689b910769179a3deb8b7355fe3ceddc5ee29aa
AzizAlhaj <aziz.alhaj96@gmail.com>
2024-09-05 12:53:28 +0300

    first commit

---
+++ wwwroot/Template/js/services-dropdown.js
@@ -0,0 +1,7 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('servicesNav').addEventListener('click', function (event) {
+        if (event.target === this) {
+            window.location.href = '/Services/Index';
+        }
+    });
+});
